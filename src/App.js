f710134a54990143741f0c7d4643ea96a7bd1a83
Kacper <kacper.barabasz@wp.pl>
2022-05-03 19:10:22 +0200

    Add basic page structure, add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
 const App = () => {
-  return <h1>Portfolio</h1>;
+  return (
+    <>
+      <h1>Logo</h1>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
